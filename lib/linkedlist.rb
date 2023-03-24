6062d354ce8aea7e9149a9a87cbcf679c3590f46
Jorja Fleming <aset284@gmail.com>
2023-03-24 17:09:57 -0600

    Add count node and string to my linked list, assignment

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -1,9 +1,35 @@
 class LinkedList
  
   attr_reader :head
-
+  
   def initialize (head = nil)
     @head = head
   end
 
+  def append(data)
+    @head = Node.new(data)
+    @head.data
+  end
+  
+  def count
+    count = 0
+    current_node = @head
+    while current_node != nil
+      count = count + 1
+      current_node = current_node.next_node
+    end
+    return count
+  end
+  
+  def to_string
+    string = ""
+    current_node = @head
+    while current_node != nil
+      string = string + current_node.data
+      current_node = current_node.next_node
+    end
+    return string
+
+  end
+  
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -6,18 +6,32 @@ RSpec.describe LinkedList do
 
   it "exist" do
     list = LinkedList.new
-    expect(list).to be_an_instance_of(LinkedList)
+    
+    node = Node.new("plop")
+      expect(list).to be_an_instance_of(LinkedList)
   end
 
-  it "has a head" do
+   it "has a head" do
     list = LinkedList.new(nil)
     expect(list.head).to eq(nil)
   end
 
-  it "can be append" do
+  it "can be appended" do
+    list = LinkedList.new(nil)
+    expect(list.append("doop")).to eq("doop")
+    
+  end
+
+  it "can count nodes" do
     list = LinkedList.new(nil)
     list.append("doop")
-    expect(list.head).to eq(nil)
+    list.count
+    expect(list.count).to eq(1)
   end
-
+  it "can make a string" do
+    list = LinkedList.new(nil)
+    list.append("doop")
+    list.to_string
+    expect(list.to_string). to eq("doop")
+  end
 end
